45d89d34e3a0db9ca59ce7f73204d54e19d1802e
Hepestoo <teckalan2@gmail.com>
2026-07-07 10:39:41 -0500

    feat: añadir validacion de email

--- cliente.js
+++ cliente.js
@@ -1,9 +1,14 @@
 // cliente.js
-// Estructura inicial del cliente
+// Cliente con validación de email
 
 function Cliente(nombre, email) {
   this.nombre = nombre;
   this.email = email;
 }
+
+Cliente.prototype.validarEmail = function () {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(this.email);
+};
 
 module.exports = Cliente;
