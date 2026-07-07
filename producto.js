4aeb779111d2413e9ff91a90bcb183e0abf9a0b9
Ajvera2000 <ajb.vera@yavirac.edu.ec>
2026-07-07 10:38:12 -0500

    feat: añadir precios a producto

--- producto.js
+++ producto.js
@@ -1,12 +1,19 @@
 // producto.js
 class Producto {
-    constructor(id, nombre) {
+    constructor(id, nombre, precio Base) {
         this.id = id;
         this.nombre = nombre;
+        this.precioBase = precioBase; // <-- Añadido en Commit 3
     }
 
     obtenerDetalles() {
-        return `Producto [${this.id}]: ${this.nombre}`;
+        return `Producto [${this.id}]: ${this.nombre} - Precio Base: $${this.precioBase}`;
+    }
+
+    // <-- Nuevo método añadido en Commit 3
+    calcularPrecioConImpuesto(porcentajeImpuesto = 21) {
+        const impuesto = this.precioBase * (porcentajeImpuesto / 100);
+        return this.precioBase + impuesto;
     }
 }
 
